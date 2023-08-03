98fc7d64c9e5ebab5c9a335e8356981b0e0a6e44
Harshan <suweeharshan@gmail.com>
2023-08-03 16:53:04 +0530

    Linear Search Recorded

--- Arrays/03_linear_Search.py
+++ Arrays/03_linear_Search.py
@@ -1,22 +1,65 @@
-def linear_search_index(arr, x):
+def linear_search(arr, target):
     for i in range(len(arr)):
-        if arr[i] == x:
+        element = arr[i]
+        if element == target:
             return i
     return -1
 
+def linear_search_range(arr, target, start, end):
+    if start < len(arr)-1:
+        for i in range(start, end+1):
+            if end > len(arr)-1:
+                return -1
+            element = arr[i]
+            if element == target:
+                return i
+    return -1
 
-def linear_search_foreach(arr, x):
-    for i in arr:
-        if i == x:
-            return 'Found'
-    return 'Not Found'
+def minimum_search(arr):
+    if len(arr) < 1:
+        return "invalid arr"
+    min_value = 9999999
+    for i in range(len(arr)):
+        curr_element = arr[i]
+        if min_value > curr_element:
+            min_value = curr_element
+    return min_value
+
+def linear_search_2d(arr, target):
+    # for row in arr:
+    #     for element in row:
+    #         if element == target:
+    #             return True
+    # return False
+
+    for i in range(len(arr)):
+        row = arr[i]
+        for j in range(len(row)):
+            element = row[j]
+            if element == target:
+                return i, j
+    return -1
 
 
-arr = [10, 13, 4, 5, 7, 8, 2, 1, 34]
-index = linear_search_index(arr, 2)
-print(f'\n\nIndex linear search found element (2) at index : {index}\n')
-index = linear_search_index(arr, 58)
-print(f'Index linear search found element (58) at index : {index}\n\n')
+def linear_search_2d_max(arr):
+    max_value = -9999999
+    for i in range(len(arr)):
+        row = arr[i]
+        for j in range(len(row)):
+            curr_element = row[j]
+            if max_value < curr_element:
+                max_value = curr_element
+    return max_value
 
-print(f'ForEach Search for element (2) in array : {linear_search_foreach(arr, 2)}\n')
-print(f'ForEach Search for element (58) in array : {linear_search_foreach(arr, 58)}\n\n')
+
+arr = [
+    [2, 4, 6],
+    [3, 6, 9],
+    [4, 8, 12, 16],
+    [7, 9, 13, 970, 103]
+]
+target = 104
+
+
+index = linear_search_2d_max(arr)
+print(f'minimum element: {index}')
